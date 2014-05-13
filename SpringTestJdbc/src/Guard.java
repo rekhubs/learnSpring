a53e11ce0b149e771e80e10b774148e9c00ef7f8
Lin Zhao <rek01@126.com>
2014-05-13 15:00:58 +0800

    aspect annotation

--- SpringTestJdbc/src/Guard.java
+++ SpringTestJdbc/src/Guard.java
@@ -1,13 +1,29 @@
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
 /**
  * Created by zhao lin on 5/12/2014.
  */
+@Aspect
 public class Guard {
 
-    public void checkUser(){
+    private String userId;
+
+    @Pointcut( "execution(* UserDao.getUsernameById(String)) && args(userId)")
+    public void getUsernameById(String userId){}
+
+    @Before("getUsernameById(userId)")
+    public void checkUser(String userId){
+        this.userId = userId;
         System.out.println("Stop! Who are you?");
     }
 
-    public void letGo(){
+    @After("getUsernameById(userId)")
+    public void letGo(String userId){
+        this.userId = userId;
+        System.out.println("Let me check your id....." + userId + ".....");
         System.out.println("OK, you can go now.");
     }
 }
